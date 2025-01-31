bd5132aa3ba1c2508182e430b4b9990088e6ebf7
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-01-31 23:16:02 +0530

    Add Cors to you apis

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,22 @@ import OpenAI from 'openai';
 import { Hono } from 'hono';
 import { cors } from 'homo/cors'; 
 
+type Bindings = {
+	OPEN_AI_KEY: string;
+	AI: Ai;
+};
 
 const app = new Hono<{ Bindings: Bindings}>();
+
+
+app.use(
+	'/*',
+	cors({
+		origin: '*',
+		allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'],
+		allowMethods: ['POST', 'GET', 'OPTIONS', 'PUT'],
+		exposeHeader: ['Content-Length', 'X-Kuma-Revision'],
+		maxAge: 600,
+		credentials: true,
+	})
+)
